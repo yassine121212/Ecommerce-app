a11ac3bf62891190f581612b8e30f56daa72c8cf
yassine121212 <elouazzaniyassine87@gmail.com>
2022-06-19 11:10:10 +0100

    Add Components ~~

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from "react";
- 
+import { Product,FooterBanner,HeroBanner } from "../components";
+  
 export default function Home() {
   return (
     <div>
+      <HeroBanner/>
       <p>home</p>
+      <FooterBanner/>
     </div>
   )
 }
