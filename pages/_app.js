df26754d388da5907b516f109cfe6fec4c6469cd
yassine121212 <elouazzaniyassine87@gmail.com>
2022-06-20 04:27:42 +0100

    Add Context to Project

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@ import React from 'react';
  
 import { Layout } from '../components';
 import '../styles/globals.css';
- 
+import {StateContext} from "../context/StateContext"
 function MyApp({ Component, pageProps }) {
   return (
+
+   <StateContext>
        <Layout>
          <Component {...pageProps} />
       </Layout>
+   </StateContext>
    )
 }
 
